7f2350ab11b7ab2c1b858958533c7f1b517aee22
freddie.wanah@gmail.com <han@karma.wiki>
2020-03-14 11:44:01 +0800

    Updates

---
+++ precache-manifest.1f725b56d511c68e2a48fed3e6d5f5df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfde643f24c28ec28bc040618de8115",
+    "url": "/2019-ncov/index.html"
+  },
+  {
+    "revision": "98f42580f4d185658775",
+    "url": "/2019-ncov/static/css/main.d80b99bd.chunk.css"
+  },
+  {
+    "revision": "0bb1aed042662583e690",
+    "url": "/2019-ncov/static/js/4.f4879a5f.chunk.js"
+  },
+  {
+    "revision": "f974136ba8cabd65a5414d669e412016",
+    "url": "/2019-ncov/static/js/4.f4879a5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5762df55398b9fc64c1",
+    "url": "/2019-ncov/static/js/5.e1d01352.chunk.js"
+  },
+  {
+    "revision": "3085481684233ebea28e",
+    "url": "/2019-ncov/static/js/data.de8f0214.chunk.js"
+  },
+  {
+    "revision": "98f42580f4d185658775",
+    "url": "/2019-ncov/static/js/main.970961bc.chunk.js"
+  },
+  {
+    "revision": "c9344e0fbbeb9a845527122bf8038412",
+    "url": "/2019-ncov/static/js/main.970961bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c19d722d42000bcd3c12",
+    "url": "/2019-ncov/static/js/react.29f7dc7d.chunk.js"
+  },
+  {
+    "revision": "9300ecbebf391c6c2038edb15bc7e2cb",
+    "url": "/2019-ncov/static/js/react.29f7dc7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aeb3c8e09204da21886",
+    "url": "/2019-ncov/static/js/runtime-main.0ae7628e.js"
+  },
+  {
+    "revision": "487fdc083f77ed81c35581dfc03e8bb9",
+    "url": "/2019-ncov/static/media/feiyan.487fdc08.jpeg"
+  }
+]);
