c360fadfd92d6141643ecf4fc6053562531f65cf
freddie.wanah@gmail.com <han@karma.wiki>
2020-03-15 09:48:52 +0800

    Updates

---
+++ precache-manifest.59eccc76ac1df67a4e3a3044b7791f01.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afbe7738240439b06e28da108375bfc",
+    "url": "/covid-19-au.github.io/index.html"
+  },
+  {
+    "revision": "9ed6210949c351fb6958",
+    "url": "/covid-19-au.github.io/static/css/main.cfbe5951.chunk.css"
+  },
+  {
+    "revision": "e5baa543f69185637552",
+    "url": "/covid-19-au.github.io/static/js/4.f8b5dbc6.chunk.js"
+  },
+  {
+    "revision": "573c03b54bf739ab0b9029188d0efb17",
+    "url": "/covid-19-au.github.io/static/js/4.f8b5dbc6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4db736d0aa6882d7bec6",
+    "url": "/covid-19-au.github.io/static/js/5.bd2e051b.chunk.js"
+  },
+  {
+    "revision": "1f48f567c4d88fea7b3c",
+    "url": "/covid-19-au.github.io/static/js/data.2cd7feba.chunk.js"
+  },
+  {
+    "revision": "9ed6210949c351fb6958",
+    "url": "/covid-19-au.github.io/static/js/main.f9d2fef8.chunk.js"
+  },
+  {
+    "revision": "53a331e0e054271ed2900859ef0c5b5f",
+    "url": "/covid-19-au.github.io/static/js/main.f9d2fef8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51e15b093238417e14ed",
+    "url": "/covid-19-au.github.io/static/js/react.c2e22786.chunk.js"
+  },
+  {
+    "revision": "9300ecbebf391c6c2038edb15bc7e2cb",
+    "url": "/covid-19-au.github.io/static/js/react.c2e22786.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fa389f0430cd024bf65",
+    "url": "/covid-19-au.github.io/static/js/runtime-main.72af3ef2.js"
+  },
+  {
+    "revision": "487fdc083f77ed81c35581dfc03e8bb9",
+    "url": "/covid-19-au.github.io/static/media/feiyan.487fdc08.jpeg"
+  }
+]);
