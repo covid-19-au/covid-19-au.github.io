bfac456eb598353777e26df0468a5e367ec240c9
freddie.wanah@gmail.com <han@karma.wiki>
2020-03-17 04:01:17 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65f5311e83ca889cd54a6bd348cbcee5.js"
+  "/precache-manifest.b62194165a434122daba931cddcd3190.js"
 );
 
 self.addEventListener('message', (event) => {
