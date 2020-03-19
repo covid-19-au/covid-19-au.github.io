5a4ddba31927a6f3be9a89ef964b88f263b447e8
Han <han@karma.wiki>
2020-03-19 11:06:05 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e7ec6187819d9ea442440556f62a33d.js"
+  "/precache-manifest.0a7a18eeb952899fb13c9eee2c1c1a76.js"
 );
 
 self.addEventListener('message', (event) => {
