a4246d5c288a07c26897019f4ee39c920c9eccc3
Paldeep <penguinpp93@gmail.com>
2020-03-19 21:19:08 +1100

    Scrollable timeline implemented and optimised

--- src/NewsTimeline.js
+++ src/NewsTimeline.js
@@ -9,34 +9,60 @@ const timelineNews = jsonNews["news"]
 const updateTime = jsonNews["updateTime"]
 
 const listStyles = {
-    maxHeight: 400,
+    maxHeight: 500,
     position: 'relative',
     overflow: 'auto',
-    minHeight: 400,
+    minHeight: 500,
     marginRight: 0,
     padding: 0
 }
 
-const colourList = []
+const newestColour = "#8e44ad"
+const middleColour = "#ec7063"
+const oldestColour = "#f5b7b1"
+
+
+const newsLength = timelineNews.length
+
+const newestNews = timelineNews.slice(0, newsLength / 3)
+const middleNews = timelineNews.slice(newsLength / 3, newsLength * 2 / 3)
+const oldestNews = timelineNews.slice(newsLength * 2 / 3, newsLength)
 
 function NewsTimeline() {
 
-    const x = 5
     return (
         <div className="card">
             <h2>News Timeline</h2>
             <Timeline lineColor={'#ddd'}>
                 <List style={listStyles}>
-                    {timelineNews.map(news =>
+                    {newestNews.map(news =>
                         <TimelineItem
                             key={timelineNews.indexOf(news)}
                             dateText={news.date + " " + news.time}
-                            dateInnerStyle={{ background: '#cb4335' }}
-                            style={{ color: '#61b8ff' }}>
+                            dateInnerStyle={{ background: "#cb4335" }}>
                             <a href={news.url}> <h4 style={{ color: ' #2980b9 ' }} >{news.title}</h4></a>
                             <h5 style={{ color: ' #5d6d7e ' }}>{news.source}</h5>
                         </TimelineItem>)
                     }
+                    {middleNews.map(news =>
+                        <TimelineItem
+                            key={timelineNews.indexOf(news)}
+                            dateText={news.date + " " + news.time}
+                            dateInnerStyle={{ background: "#ec7063" }}>
+                            <a href={news.url}> <h4 style={{ color: ' #2980b9 ' }} >{news.title}</h4></a>
+                            <h5 style={{ color: ' #5d6d7e ' }}>{news.source}</h5>
+                        </TimelineItem>)
+                    }
+                    {oldestNews.map(news =>
+                        <TimelineItem
+                            key={timelineNews.indexOf(news)}
+                            dateText={news.date + " " + news.time}
+                            dateInnerStyle={{ background: "#f5b7b1" }}>
+                            <a href={news.url}> <h4 style={{ color: ' #2980b9 ' }} >{news.title}</h4></a>
+                            <h5 style={{ color: ' #5d6d7e ' }}>{news.source}</h5>
+                        </TimelineItem>)
+                    }
+
                 </List>
             </Timeline >
             <span className="due">Time in AEDT, last updated at {updateTime[0].time}</span>
