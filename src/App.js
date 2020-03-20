8ecf02a93efe559ed935c72d1f7ed98c192817ed
BoxHezi <547181293@qq.com>
2020-03-20 16:50:21 +1100

    hide about card

--- src/App.js
+++ src/App.js
@@ -713,9 +713,9 @@ function App() {
           <Grid item xs={12} sm={12} md={10} lg={6} xl={5}>
             <Flights flights={flights} />
           </Grid>
-          <Grid item xs={12} sm={12} md={10} lg={6} xl={5}>
+          {/* <Grid item xs={12} sm={12} md={10} lg={6} xl={5}>
             <About />
-          </Grid>
+          </Grid> */}
           <Grid item xs={12}>
             <ExposureSites />
           </Grid>
