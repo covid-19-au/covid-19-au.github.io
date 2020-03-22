00bead8f6c5e8910980e68b621328b2a587da1b7
Han <han@karma.wiki>
2020-03-22 18:25:03 +0800

    add new features

--- src/ConfirmedMap.js
+++ src/ConfirmedMap.js
@@ -133,7 +133,7 @@ class MbMap extends React.Component {
                 flexDirection: 'column',
                 height: '520px'
             }}>
-                <h2>Hospitals & Cases Map</h2>
+                <h2>Hospital & Case Map</h2>
 
                 <div style={style} ref={el => this.mapContainer = el} >
                     {/*{*/}
