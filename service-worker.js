4e38567c9f783d7f3b25bb73876ffa0dfb333b7c
freddiewanah <freddie.wanah@gmail.com>
2020-03-23 03:46:30 +0000

    Deploying to master from  - fa88c12d8e601522c8ac51d62bbeb4881aba9c21 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b517dc0153ddb2408b78da3c8bb107.js"
+  "/precache-manifest.2eebe0a4e4de0a36e241e1c673bea2de.js"
 );
 
 self.addEventListener('message', (event) => {
