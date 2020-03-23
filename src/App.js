68cec997df0ca50e959f067834eedf85c0629580
Paldeep <penguinpp93@gmail.com>
2020-03-23 20:29:07 +1100

    remove tested note

--- src/App.js
+++ src/App.js
@@ -748,9 +748,8 @@ function HomePage({
               style={{ fontSize: "70%", float: "left", paddingLeft: 0 }}
               className="due"
             >
-              Numbers in brackets indicate daily increase
-              *Number of tested cases is updated daily. <br />
-              <br />**Note that under National Notifiable Diseases Surveillance System reporting requirements, cases are reported based on their Australian jurisdiction of residence rather than where they were detected. For example, a case reported previously in the NT in a NSW resident is counted in the national figures as a NSW case.
+              Numbers in brackets indicate daily increase <br />
+              *Note that under National Notifiable Diseases Surveillance System reporting requirements, cases are reported based on their Australian jurisdiction of residence rather than where they were detected. For example, a case reported previously in the NT in a NSW resident is counted in the national figures as a NSW case.
 
 
             </span>
