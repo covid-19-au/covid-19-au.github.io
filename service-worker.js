3e676b7665e100bff22d0e2c84d9ea68f72b7759
covid-19-au <covid-19-au@users.noreply.github.com>
2020-03-24 04:08:27 +0000

    Deploying to master from  - dc42757a7088e99f3317b2593875a7b48be4ccd6 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b274c57ad5b41239784974208fd559e1.js"
+  "/precache-manifest.9f884db7a55a9b016b2337c04652e114.js"
 );
 
 self.addEventListener('message', (event) => {
