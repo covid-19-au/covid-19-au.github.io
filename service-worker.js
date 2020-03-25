6f108a7f765737b09dd5e96814bf055b8cb48e1e
PenguinPP <PenguinPP@users.noreply.github.com>
2020-03-25 12:06:44 +0000

    Deploying to master from  - 6af7ca0c44217b67bc19d2c34e0f0df83b4646af 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bb0d5c0e4d404099e39071fe993aa39.js"
+  "/precache-manifest.9e7c1bbd9f3ec9e8926c3d7adcf3e65a.js"
 );
 
 self.addEventListener('message', (event) => {
