c7277f9681b9cbfb14962f8248d435b0ae85e3a9
Han <han@karma.wiki>
2020-03-26 10:08:05 +0800

    update comma

--- src/App.js
+++ src/App.js
@@ -442,6 +442,10 @@ function Fallback(props) {
   );
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function Area({ area, onChange, data }) {
   let totalRecovered = 0;
   for (let i = 0; i < data.length; i++) {
@@ -470,15 +474,15 @@ function Area({ area, onChange, data }) {
           <strong>{x[0]}</strong>
         </div>
         <div className="confirmed">
-          <strong>{x[1]}</strong>{x[0] === 'NSW' || x[0] === 'NT' || x[0] === 'TAS' ? '*' : null}&nbsp;{(x[1] - lastTotal[x[0]][0]) > 0 ? `(+${x[1] - lastTotal[x[0]][0]})` : null}
+          <strong>{numberWithCommas(x[1])}</strong>{x[0] === 'NSW' || x[0] === 'NT' || x[0] === 'TAS' ? '*' : null}&nbsp;{(x[1] - lastTotal[x[0]][0]) > 0 ? `(+${x[1] - lastTotal[x[0]][0]})` : null}
         </div>
         <div className="death">
-          <strong>{x[2]}</strong>&nbsp;{(x[2] - lastTotal[x[0]][1]) > 0 ? ` (+${x[2] - lastTotal[x[0]][1]})` : null}
+          <strong>{numberWithCommas(x[2])}</strong>&nbsp;{(x[2] - lastTotal[x[0]][1]) > 0 ? ` (+${x[2] - lastTotal[x[0]][1]})` : null}
         </div>
         <div className="cured">
-          <strong>{x[3]}</strong>&nbsp;{(x[3] - lastTotal[x[0]][2]) > 0 ? `(+${x[3] - lastTotal[x[0]][2]})` : null}
+          <strong>{numberWithCommas(x[3])}</strong>&nbsp;{(x[3] - lastTotal[x[0]][2]) > 0 ? `(+${x[3] - lastTotal[x[0]][2]})` : null}
         </div>
-        <div className="tested">{x[4]}</div>
+        <div className="tested">{numberWithCommas(x[4])}</div>
       </div>
     ));
   };
