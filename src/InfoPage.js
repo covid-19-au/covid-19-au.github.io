6d51c43931f7eefbb71c8b05ffae510071cfd166
Paldeep <penguinpp93@gmail.com>
2020-03-28 03:00:32 +1100

    info page add

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -28,7 +28,13 @@ function Information({ hospitalData, columns }) {
             <ReactPlayer alt="Stupid Spaceman Virus Proof House" className="formatMedia" url="https://www.youtube.com/watch?v=115m7ji5mdY" controls={true} config={{ youtube: { playerVars: { showinfo: 1 } } }} />
             <small className="mediaText">Watch Quentin spend 24 hours in the "world's most virus proof house!</small>
           </div>
+          <br />
+
         </div>
+        <p style={{ textAlign: "center" }}>We will be regularly sharing fun and interesting things in this section as we believe it is good to spread some positivity in times like these!</p>
+
+        <p style={{ textAlign: "center" }}>If you have something that you would like us to share, you can submit it <a style={{ color: "#3366BB" }} target="_blank"
+          rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLScPl8U9tILO2wD1xbtkz1pDTW0wBcAlcIb3cnJvnvUahAZEuw/viewform?usp=sf_link">{"here!"}</a> </p>
       </div>
       <div className="card" >
         <h2 className="responsiveH2">Informative Media</h2>
