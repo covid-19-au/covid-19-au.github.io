c25550bc6eeee54f955018e5191477ec3ac63452
Paldeep <penguinpp93@gmail.com>
2020-03-29 22:29:23 +1100

    added cards

--- src/TopNews.js
+++ src/TopNews.js
@@ -33,7 +33,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const listStyles = {
-    maxHeight: 500,
+    maxHeight: 450,
     position: 'relative',
     overflow: 'auto',
     minHeight: 100,
@@ -48,68 +48,71 @@ function TopNews() {
     return (
 
         <div className="card">
-            <h2>Top News this Week</h2>
+            <h2>Top News</h2>
 
             <List style={listStyles}>
                 {newsData.map(news =>
-                    < ExpansionPanel style={{ boxShadow: "none" }} key={uuid()} >
-                        < ExpansionPanelSummary expandIcon={< ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                            style={{ textAlign: "left", padding: "0px" }}>
-                            <ListItem alignItems="flex-start" key={uuid()}>
-                                <ListItemAvatar>
-                                    <Avatar style={{ marginTop: "0.3rem", marginRight: "1rem", width: "48px", height: "48px" }} src={require("./img/newsLogos/" + news.source + ".jpg")} />
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={
-                                        <React.Fragment>
-                                            <Typography
-                                                variant="body1"
-                                                className={classes.inline}
-                                                color="textPrimary"
-                                            >
-                                                {news.title}
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
-                                    secondary={
-                                        <React.Fragment>
-                                            <Typography
-                                                component="span"
-                                                variant="body2"
-                                                className={classes.inline}
-                                                color="textPrimary"
-                                            >
-                                                {news.date}
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
-                                />
-                            </ListItem>
-                        </ExpansionPanelSummary>
-                        <ExpansionPanelDetails style={{ textAlign: "left", marginLeft: "1em", padding: "0px", flexDirection: "column" }}>
-                            <Typography>
-                                {news.summary}
-                            </Typography>
-                            <div align="right" style={{ marginRight: "2rem" }}>
-                                <IconButton
-                                    variant="contained"
-                                    className={classes.button}
-                                    href={news.link}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    /*startIcon={<LinkIcon />}*/
-                                    size="medium"
-                                    style={{
-                                        backgroundColor: "#e3f3ff"
-                                    }}
-                                ><LinkIcon />
-                                </IconButton>
-                            </div>
+                    <div className="card" style={{ padding: "5px" }}>
+                        <ListItem alignItems="flex-start" key={uuid()}>
+                            < ExpansionPanel style={{ boxShadow: "none" }} >
+                                < ExpansionPanelSummary expandIcon={< ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                    style={{ textAlign: "left", padding: "0px" }}>
+                                    <ListItemAvatar>
+                                        <Avatar style={{ marginTop: "0.rem", marginRight: "1rem", width: "48px", height: "48px" }} src={require("./img/newsLogos/" + news.source + ".jpg")} />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    variant="body1"
+                                                    className={classes.inline}
+                                                    color="textPrimary"
+                                                >
+                                                    {news.title}
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    className={classes.inline}
+                                                    color="textPrimary"
+                                                >
+                                                    {news.date}
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                    />
 
-                        </ExpansionPanelDetails>
-                    </ ExpansionPanel>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails style={{ textAlign: "left", marginLeft: "1em", padding: "0px", flexDirection: "column" }}>
+                                    <Typography>
+                                        {news.summary}
+                                    </Typography>
+                                    <div align="right" style={{ marginRight: "2rem" }}>
+                                        <IconButton
+                                            variant="contained"
+                                            className={classes.button}
+                                            href={news.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            /*startIcon={<LinkIcon />}*/
+                                            size="medium"
+                                            style={{
+                                                backgroundColor: "#e3f3ff"
+                                            }}
+                                        ><LinkIcon />
+                                        </IconButton>
+                                    </div>
+
+                                </ExpansionPanelDetails>
+                            </ ExpansionPanel>
+                        </ListItem>
+                    </div>
                 )
                 }
 
