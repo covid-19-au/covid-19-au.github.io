71538c0855138792cc67067e10efe1d58d1cabc6
Han <han@karma.wiki>
2020-03-29 21:22:52 +0800

    update

---
+++ src/FAQPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FAQ from "./faq";
+import Grid from "@material-ui/core/Grid";
+
+
+export default function FAQPage() {
+    return (
+        <Grid item xs={11} sm={11} md={10}>
+            <FAQ />
+        </Grid>
+    )
+}
+
--- src/fallback.js
+++ src/fallback.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactGA from "react-ga";
-// import {Link} from 'react-router-dom';
+import {A} from 'hookrouter';
+
 // ReactGA.initialize("UA-160673543-1",{gaoOptions:{siteSpeedSampleRate: 100}});
 function Fallback(props) {
     return (
@@ -39,7 +40,7 @@ function Fallback(props) {
                     &nbsp;Feedback
                 </a>
 
-
+                <A href="/faq">
                 <button type="button" className="btn btn-light btn-sm m-1" onClick={() => {
                     ReactGA.event({category: 'Fallback',action: "faq"});
                     props.setNav("About");
@@ -54,16 +55,18 @@ function Fallback(props) {
                     </svg>
                     &nbsp;FAQ
                 </button>
+                </A>
+              <div>
+                <a role="button" aria-disabled="true" target="_blank" rel="noopener noreferrer" className="btn btn-light btn-sm m-1" href="https://github.com/covid-19-au/covid-19-au.github.io">
+                  <svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="m12 .5c-6.63 0-12 5.28-12 11.792 0 5.211 3.438 9.63 8.205 11.188.6.111.82-.254.82-.567 0-.28-.01-1.022-.015-2.005-3.338.711-4.042-1.582-4.042-1.582-.546-1.361-1.335-1.725-1.335-1.725-1.087-.731.084-.716.084-.716 1.205.082 1.838 1.215 1.838 1.215 1.07 1.803 2.809 1.282 3.495.981.108-.763.417-1.282.76-1.577-2.665-.295-5.466-1.309-5.466-5.827 0-1.287.465-2.339 1.235-3.164-.135-.298-.54-1.497.105-3.121 0 0 1.005-.316 3.3 1.209.96-.262 1.98-.392 3-.398 1.02.006 2.04.136 3 .398 2.28-1.525 3.285-1.209 3.285-1.209.645 1.624.24 2.823.12 3.121.765.825 1.23 1.877 1.23 3.164 0 4.53-2.805 5.527-5.475 5.817.42.354.81 1.077.81 2.182 0 1.578-.015 2.846-.015 3.229 0 .309.21.678.825.56 4.801-1.548 8.236-5.97 8.236-11.173 0-6.512-5.373-11.792-12-11.792z" fill="#212121"/>
+                  </svg>&nbsp;GitHub
+                </a>
+              </div>
 
             </div>
             <div>Template credits to: shfshanyue</div>
 
-            <div>
-                Our GitHub:{" "}
-                <a href="https://github.com/covid-19-au/covid-19-au.github.io">
-                    covid-19-au
-                </a>
-            </div>
             <div>
                 This site is developed by a{" "}
                 <a href="https://github.com/covid-19-au/covid-19-au.github.io/blob/dev/README.md">
@@ -71,7 +74,11 @@ function Fallback(props) {
                 </a>{" "}
                 from the Faculty of IT, Monash University, for non-commercial use only.
             </div>
+
             <div>
+                <svg className="bi bi-person-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"/>
+                </svg>
                 <a href="https://www.webfreecounter.com/" target="_blank" rel="noopener noreferrer">
                     <img
                         src="https://www.webfreecounter.com/hit.php?id=gevkadfx&nd=9&style=1"
