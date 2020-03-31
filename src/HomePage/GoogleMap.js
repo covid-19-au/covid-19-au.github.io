858b8ea41613e8538980125b52632a0c3c52aecd
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-03-31 18:53:28 +1100

    Change droplist to button group
    Also update

--- src/HomePage/GoogleMap.js
+++ src/HomePage/GoogleMap.js
@@ -145,19 +145,15 @@ function GoogleMap({ province, newData }) {
         loading ? <div className="loading">Loading...</div> :
             <div className="stateMap">
                 <h2> Cases by State {province ? `· ${province.name}` : false} </h2>
-                <span className="selection-grid">
-                    <NativeSelect
-                        className="mapToggle"
-                        onChange={toggleData}
-                    >
-                        <option value="confirmed-cases">Confirmed cases</option>
-                        <option value="relative-cases">Cases per million people</option>
-                        <option value="tested">Tested</option>
-                        <option value="relative-tests">Tests per million people</option>
-                        {/*<option value="deaths">Deaths</option>*/}
 
-                        <option value="test-strike">Positive test rate</option>
-                    </NativeSelect>
+                <span className="selection-grid">
+                    <div class="btn-group btn-group-sm btn-group-toggle" role="group" aria-label="Map types">
+                      <button type="button" class="btn btn-danger" value="confirmed-cases" onClick={toggleData}>CONFIRMED CASES</button>
+                      <button type="button" class="btn btn-warning" value="relative-cases" onClick={toggleData}>CASES/MILLION PEOPLE</button>
+                      <button type="button" class="btn btn-primary" value="tested" onClick={toggleData}>TESTED</button>
+                      <button type="button" class="btn btn-info" value="relative-tests" onClick={toggleData}>TESTS/MILLION PEOPLE</button>
+                      <button type="button" class="btn btn-secondary" value="test-strike" onClick={toggleData}>POSITIVE TEST RATE</button>
+                    </div>
                 </span>
 
                 <Chart
