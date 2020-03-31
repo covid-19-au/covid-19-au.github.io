076bdffa66ea58a7f6de3accf9b75e941b422c5e
Paldeep <penguinpp93@gmail.com>
2020-04-01 00:27:18 +1100

    changed acknowledgement button format

--- src/Acknowledgment.js
+++ src/Acknowledgment.js
@@ -47,10 +47,24 @@ export default function ScrollDialog() {
 
     return (
         <div>
-            <Button variant="text" size="small" style={{ padding: "0px", margin: "0px", fontSize: "60%", textTransform: "none" }} onClick={handleClickOpen('paper')}
+            <a
+                style={{
+                    display: "inline-flex"
+                }}
+                onClick={handleClickOpen('paper')}
+                className="badge badge-light"
+                target="_blank" rel="noopener noreferrer"
+                href="https://github.com/covid-19-au/covid-19-au.github.io/blob/dev/reference/reference.md"
             >
-                Acknowledgements
-                </Button>
+                <svg className="bi bi-question-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z"
+                        clipRule="evenodd" />
+                    <path
+                        d="M5.25 6.033h1.32c0-.781.458-1.384 1.36-1.384.685 0 1.313.343 1.313 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.007.463h1.307v-.355c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.562 5.516c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
+                </svg>
+                <div className="dataSource">Acknowledgements</div>
+            </a>
             <Dialog
                 open={open}
                 onClose={handleClose}
