e83144cac1f112c0d052937a2970e6be16dd479e
Paldeep <penguinpp93@gmail.com>
2020-04-03 11:47:07 +1100

    tag death count fix

--- src/HomePage/Tag.js
+++ src/HomePage/Tag.js
@@ -2,22 +2,24 @@ import React from 'react'
 import './Tag.css'
 
 function getAccessiblityDescription(number, increased, children) {
-    if(!increased) increased = 0;
-    return `There are ${number} ${children} cases.` +  (increased && increased>0 ? `They increased by ${increased}` : "");
+  if (!increased) increased = 0;
+  return `There are ${number} ${children} cases.` + (increased && increased > 0 ? `They increased by ${increased}` : "");
 }
 
-function Tag ({ children, number,fColor ,increased }) {
+function Tag({ children, number, fColor, increased }) {
 
   return (
     <div className="tag" role={"button"} aria-label={getAccessiblityDescription(number, increased, children)}>
-        {
-            increased > 0 ? <div style={{fontSize:'80%',display:'inline-flex'}}><div style={{color:`${fColor}`}}>+{increased}</div>&nbsp;today</div> : <div style={{fontSize:'80%',display:'inline-flex'}}><div>&nbsp;</div>&nbsp;</div>
-        }
-      <div style={{color:`${fColor}`,  fontSize: '1.2rem',
-          fontWeight: '600'}} className="number">
-        { number===25?46:number }
+      {
+        increased > 0 ? <div style={{ fontSize: '80%', display: 'inline-flex' }}><div style={{ color: `${fColor}` }}>+{increased}</div>&nbsp;today</div> : <div style={{ fontSize: '80%', display: 'inline-flex' }}><div>&nbsp;</div>&nbsp;</div>
+      }
+      <div style={{
+        color: `${fColor}`, fontSize: '1.2rem',
+        fontWeight: '600'
+      }} className="number">
+        {number}
       </div>
-      { children }
+      {children}
     </div>
   )
 }
