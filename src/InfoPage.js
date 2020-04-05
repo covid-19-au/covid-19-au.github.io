01cfd83bfb3b713f652dff72be1d97a78a96a111
Paldeep <penguinpp93@gmail.com>
2020-04-05 21:25:57 +1000

    DD

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -74,7 +74,7 @@ function Information({ hospitalData, columns }) {
                                                 <img
                                                     src={i1.imgLink}
                                                     alt={i1.name}
-                                                    style={{ height: "50vh" }}
+                                                    style={{ width: "98%" }}
                                                 />
 
                                             </div>
