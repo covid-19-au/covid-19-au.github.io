652db44db475623324a0b51235619b33daa12a5c
ish-joshi <ijos0001@student.monash.edu>
2020-04-05 11:48:28 +1000

    fix: description for tags accessibilty descriptions

--- src/HomePage/Stat.js
+++ src/HomePage/Stat.js
@@ -73,6 +73,7 @@ export default function Stat({
                     number={confirmedCount}
                     fColor={"#ff603c"}
                     increased={confCountIncrease}
+                    typeOfCases={"Confirmed"}
                 >
                     <button className="hoverButton" data-toggle="tooltip" data-placement="bottom" data-html="true"
                         title="<em>All confirmed cases of COVID-19 so far, including deaths and recoveries.</em>">
@@ -86,6 +87,7 @@ export default function Stat({
                     number={deadCount}
                     fColor={"#c11700"}
                     increased={deadCountIncrease}
+                    typeOfCases={"Deaths"}
                 >
                     <button className="hoverButton" data-toggle="tooltip" data-placement="bottom" data-html="true"
                         title="<em>All confirmed deaths due to COVID-19, including 1 from the Diamond Princess cruise ship.</em>">
@@ -98,6 +100,7 @@ export default function Stat({
                     number={curedCount}
                     fColor={"#00c177"}
                     increased={curedCountIncrease}
+                    typeOfCases={"Recovered"}
                 >
                     <button className="hoverButton" data-toggle="tooltip" data-placement="bottom" data-html="true"
                         title="<em>Number of people that have recovered from COVID-19.</em>">
@@ -108,6 +111,7 @@ export default function Stat({
                     number={testedCount}
                     fColor={"#007cf2"}
                     increased={testedCountIncrease}
+                    typeOfCases={"Tested"}
                 >
                     <button className="hoverButton" data-toggle="tooltip" data-placement="bottom" data-html="true"
                         title="<em>Number of people that have been tested for COVID-19.</em>">
--- src/HomePage/Tag.js
+++ src/HomePage/Tag.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Tag.css'
 
-function getAccessiblityDescription(number, increased, children) {
+function getAccessiblityDescription(number, increased, typeOfCases) {
     if(!increased) increased = 0;
-    return `There are ${number} ${children} cases.` +  (increased && increased>0 ? `They increased by ${increased}` : "");
+    if (typeOfCases.toLowerCase().indexOf("test") !== -1)
+        return `There are ${number} people who were ${typeOfCases}.` +  (increased && increased>0 ? `They increased by ${increased}` : "");
+    else
+        return `There are ${number} ${typeOfCases} cases.` +  (increased && increased>0 ? `They increased by ${increased}` : "");
 }
 
-function Tag ({ children, number,fColor ,increased }) {
+function Tag ({ children, number,fColor ,increased, typeOfCases }) {
 
   return (
-    <div className="tag" role={"button"} aria-label={getAccessiblityDescription(number, increased, children)}>
+    <div className="tag" role={"button"} aria-label={getAccessiblityDescription(number, increased, typeOfCases)}>
         {
             increased > 0 ? <div style={{fontSize:'80%',display:'inline-flex'}}><div style={{color:`${fColor}`}}>+{increased}</div>&nbsp;today</div> : <div style={{fontSize:'80%',display:'inline-flex'}}><div>&nbsp;</div>&nbsp;</div>
         }
