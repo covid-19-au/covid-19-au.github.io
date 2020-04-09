52118636b4f437e9e3ec897609cf3cc9baa2ecfd
BoxHezi <547181293@qq.com>
2020-04-09 10:27:40 +1000

    add timestamp to show update time

--- src/DataVis/StateChart.js
+++ src/DataVis/StateChart.js
@@ -43,19 +43,25 @@ const lineBarTooltip = {
 };
 
 const ageTooltip = {
-    trigger: "axis",
-    axisPointer: {
-        crossStyle: {
-            color: "#999",
-        },
+  trigger: "axis",
+  axisPointer: {
+    crossStyle: {
+      color: "#999",
     },
-    formatter: function (params) {
-        let str = '<div><p>Age: '+params[0].name+'</p></div>';
-        for(let i=0;i<params.length;i++){
-            str+=params[i].marker+' '+params[i].seriesName+': '+params[i].data+'<br/>'
-        }
-        return str
+  },
+  formatter: function (params) {
+    let str = "<div><p>Age: " + params[0].name + "</p></div>";
+    for (let i = 0; i < params.length; i++) {
+      str +=
+        params[i].marker +
+        " " +
+        params[i].seriesName +
+        ": " +
+        params[i].data +
+        "<br/>";
     }
+    return str;
+  },
 };
 
 const pieTooltip = {
@@ -456,6 +462,9 @@ function calcMaxY(logScale, maxValue) {
 }
 
 function StateChart({ state }) {
+  const statusUpdateTime = latestAusData["updatedTime"];
+  const ageGenderUpdateTime = ageGenderData["DateUpdate"];
+
   // get choosen state data
   const expectStateData = getExpectStateData(state);
 
@@ -481,7 +490,7 @@ function StateChart({ state }) {
     genderOption = setGenderOption(expectStateData);
     ageOption = setAgeOption(expectStateData);
   }
-  ReactGA.pageview("/state/"+state);
+  ReactGA.pageview("/state/" + state);
 
   if (expectStateData !== null) {
     return (
@@ -504,12 +513,18 @@ function StateChart({ state }) {
               </div>
               {renderTable(state.toUpperCase())}
             </div>
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4}>
           <div className="card">
             <h2>General Information - Bar</h2>
             <ReactEcharts option={barOption} />
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4}>
@@ -565,18 +580,27 @@ function StateChart({ state }) {
                 <div className="dataSource"></div>
               </a>
             </span>
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4} xl={4}>
           <div className="card">
             <h2>Cases by Gender</h2>
             <ReactEcharts option={genderOption} />
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4} xl={6}>
           <div className="card">
             <h2>Cases by Age Group</h2>
             <ReactEcharts option={ageOption} />
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
       </Grid>
@@ -602,12 +626,18 @@ function StateChart({ state }) {
               </div>
               {renderTable(state.toUpperCase())}
             </div>
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4}>
           <div className="card">
             <h2>General Information - Bar</h2>
             <ReactEcharts option={barOption} />
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {statusUpdateTime}
+            </span>
           </div>
         </Grid>
         <Grid item xs={11} sm={11} md={4}>
@@ -662,6 +692,9 @@ function StateChart({ state }) {
                 </svg>
                 <div className="dataSource"></div>
               </a>
+            </span>
+            <span className="due" style={{ fontSize: "80%", padding: 0 }}>
+              Last Update: {ageGenderUpdateTime}
             </span>
           </div>
         </Grid>
