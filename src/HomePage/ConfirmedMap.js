03deba668cc2d6ac126b3dac3f82f813a3a8fea5
Han <han@karma.wiki>
2020-04-09 23:18:31 +0800

    update

--- src/HomePage/ConfirmedMap.js
+++ src/HomePage/ConfirmedMap.js
@@ -621,7 +621,7 @@ class MbMap extends React.Component {
                     <span className="key"><img src={hospitalImg} /><p>Hospital or COVID-19 assessment centre</p></span>
                     <span className="key"><img src={confirmedOldImg} /><p>Case over {oldCaseDays} days old</p></span>
                     <span className="key"><img src={confirmedImg} /><p>Recently confirmed case(not all, collecting)</p></span>
-                    <span className="Key"><p>*City-level data is only present for NSW, VIC, and WA, HHS Data for QLD. Other states are being worked on.</p></span>
+                    <span className="Key"><p>*City-level data is only present for <strong>ACT</strong>, <strong>NSW</strong>, <strong>VIC</strong>, and <strong>WA</strong>, HHS Data for <strong>QLD</strong>. Other states are being worked on.</p></span>
                     <span className="key" style={{ alignSelf: "flex-end", marginTop: "0.5rem" }}>
                         Markers:&nbsp;<ButtonGroup size="small" aria-label="small outlined button group">
                             <Button style={this.state.showMarker ? activeStyles : inactiveStyles} disableElevation={true} onClick={() => this.handleClickOn()}>On</Button>
