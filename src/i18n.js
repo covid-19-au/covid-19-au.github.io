d0276b311bb45fdba0962b582bfe86aa7bf67c6e
melonbun <gu.karlan@gmail.com>
2020-04-10 18:03:29 +1000

    Nav bar translation update

--- src/i18n.js
+++ src/i18n.js
@@ -4,9 +4,17 @@ import { initReactI18next } from 'react-i18next';
 import infoPage_en from "./translations/en/infoPage.json";
 import homePage_en from "./translations/en/homePage.json";
 import faq_en from "./translations/en/faq.json";
-import infoPage_zh from "./translations/zh/infoPage.json";
-import infoPage_ko from "./translations/ko/infoPage.json";
+import nav_en from "./translations/en/nav.json"
 
+import infoPage_zh from "./translations/zh/infoPage.json";
+import homePage_zh from "./translations/zh/homePage.json";
+import faq_zh from "./translations/zh/faq.json";
+import nav_zh from "./translations/zh/nav.json"
+
+import infoPage_ko from "./translations/ko/infoPage.json";
+import homePage_ko from "./translations/ko/homePage.json";
+import faq_ko from "./translations/ko/faq.json";
+import nav_ko from "./translations/ko/nav.json"
 
 const allowedLanguages = ['en', 'ko','zh'];
 
@@ -29,14 +37,21 @@ i18n
         en: {
             infoPage: infoPage_en,
             homePage: homePage_en,
-            faq: faq_en           
+            faq: faq_en,
+            nav: nav_en           
             //the key to call the json is infoPage:dailyDistraction.title
         },
         zh: {
             infoPage: infoPage_zh,
+            homePage: homePage_zh,
+            faq: faq_zh,    
+            nav: nav_zh 
         },
         ko: {
           infoPage: infoPage_ko,
+          homePage: homePage_ko,
+          faq: faq_ko,  
+          nav: nav_ko   
         },        
     },
 
