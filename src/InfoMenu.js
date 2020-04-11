a61ef156743b3474df815ddaa9f31233b3c70a07
Paldeep <penguinpp93@gmail.com>
2020-04-11 16:25:55 +1000

    info menu created

---
+++ src/InfoMenu.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Fab from '@material-ui/core/Fab';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
+export default function InfoDrawer() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            {['left', 'right', 'top', 'bottom'].map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <Fab size="medium" color="secondary" aria-label="menu" onClick={toggleDrawer(anchor, true)} style={{
+                        position: "fixed", zIndex: 95,
+                        bottom: "3rem",
+                        right: "2rem", backgroundColor: "#8ccfff"
+                    }}>
+                        <MenuIcon style={{ color: "black" }} />
+                    </Fab>
+                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                        {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            ))}
+        </div>
+    );
+}
--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     useTable,
     useFilters,
@@ -18,10 +18,28 @@ import mapDataHos from "./data/mapdataHos";
 import ReactGA from "react-ga";
 import ReactHtmlParser from 'react-html-parser';
 import { A } from 'hookrouter';
-
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Fab from '@material-ui/core/Fab';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 
 // Info page to present information about the virus.
+
+
+
 export default function InfoPage({ columns }) {
+
+    const [drawerOpen, setDrawerOpen] = useState(false)
 
     const stateAbrev = {
         "Victoria": "VIC",
@@ -53,10 +71,69 @@ export default function InfoPage({ columns }) {
     )
 }
 
-function Information({ hospitalData, columns }) {
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
+function InfoDrawer() {
+    const classes = useStyles();
+    const [state, setState] = React.useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState(open);
+    };
+
+    const list = () => (
+        <div
+            className={clsx(classes.list)}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                <ListItem>
+                    <Typography style={{ alignContent: "center" }} >SUP</Typography>
+                </ListItem>
+            </List>
+        </div>
+    );
+
     return (
         <div>
+            <React.Fragment>
+                <Fab size="medium" color="secondary" aria-label="menu" onClick={toggleDrawer(true)} style={{
+                    position: "fixed", zIndex: 95,
+                    bottom: "3rem",
+                    right: "2rem", backgroundColor: "#8ccfff"
+                }}>
+                    <MenuIcon style={{ color: "black" }} />
+                </Fab>
+                <Drawer anchor={'bottom'} open={state} onClose={toggleDrawer(false)}>
+                    {list()}
+                </Drawer>
+            </React.Fragment>
+        </div>
+    );
+}
+
+function Information({ hospitalData, columns }) {
+
+
+    return (
+
+        <div>
+
             <div className="card">
+                <InfoDrawer></InfoDrawer>
                 <h2 className="responsiveH2">Daily Distractions</h2>
                 {dailyFun.dailyFunStuff.map(stuff => (
                     stuff.type === "motivation" ? (
