883e09690a6db42a5524e326bfd48dac5ebf02fb
Andrew Zhang <andrew.zhang9799@gmail.com>
2020-04-12 20:47:31 +1000

    add blog with comments

--- src/BlogPage/Blog.js
+++ src/BlogPage/Blog.js
@@ -1,15 +1,140 @@
-import React from "react";
+import React, {Component} from "react";
+import blogData from "../data/blogPosts";
+import uuid from "react-uuid";
+import "./Blog.css";
+import {A} from "hookrouter";
+
+// Styling imports
 import Grid from "@material-ui/core/Grid";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 
+const filterOptions = createFilterOptions({
+    matchFrom: 'start',
+    stringify: (option) => option.title,
+});
 
-function Blog(props) {
-    return(
-        <Grid item xs={11} sm={11} md={10}>
-            <div className="card">
-                <h2 className="responsiveH2">Blog</h2>
-            </div>
-        </Grid>
-    )
+// Replace all special characters with _
+function replaceAll(str, replace) {
+    return str.replace(/[^A-Z0-9]+/ig, replace);
+}
+
+const filterPickList = [
+    {title: 'Informative'},
+    {title: 'Fun'}
+];
+
+class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentFilter: ''
+        }
+    }
+
+    onChange = e => {
+        this.setState({currentFilter: e.target.innerHTML});
+    }
+
+    onClick = () => {
+        window.scrollTo(0, 0);
+    }
+
+    filterBlogs = (allBlogs, tag) => {
+        let opts = filterPickList.map(picks => picks.title);
+        return allBlogs.filter(blog => {
+            if (!opts.includes(tag)) {
+                return blog;
+            } else if (blog.tag === tag) {
+                return blog;
+            }
+        });
+    }
+
+    render() {
+        return(
+            <Grid item xs={11} sm={11} md={10}>
+                <div className="card">
+                    <h2 className="responsiveH2">Blog</h2>
+                    <Grid container direction="row" justify="center" alignItems="center" style={{marginBottom: '2em'}}>
+                        <Grid item style={{display: 'flex', justifyContent: 'center'}} xs={10} sm={8} md={6} lg={4} xl={2}>
+                            <Autocomplete
+                                openOnFocus
+                                id="filter-demo"
+                                options={filterPickList}
+                                getOptionLabel={(option) => option.title}
+                                filterOptions={filterOptions}
+                                onChange={this.onChange}
+                                style={{ width: 220 }}
+                                renderInput={(params) => <TextField {...params} label="Filter a Genre" variant="outlined" />}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" justify="center" alignItems="center">
+                        {this.filterBlogs(blogData.blogPosts, this.state.currentFilter).map(blog => (
+                            <Card key={uuid()} className="classesRoot cardFormat">
+                                <A onClick={this.onClick} className="noHover" href={`/blog/${replaceAll(blog.title, '_')}`}>
+                                    <CardActionArea>
+                                        <Grid container direction="row" justify="center" >
+                                            <Grid className="responsiveCardTop" item xs={12} sm={12} md={6}>
+                                                <CardContent style={{paddingBottom: 0, height: '100%'}}>
+                                                    <div style={{height: "80%", marginBottom: 'auto'}}>
+                                                        <Typography gutterBottom className="classesH2" variant="h5" component="h2">
+                                                        {blog.title}
+                                                        </Typography>
+                                                        <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                                                        {blog.summary}
+                                                        </Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Typography gutterBottom variant="overline" color="textSecondary" component="p">
+                                                        {blog.date}
+                                                        </Typography>
+                                                    </div>
+                                                </CardContent>
+                                            </Grid>
+                                            <Grid item xs={12} sm={12} md={6}>
+                                                <CardMedia
+                                                    component="img"
+                                                    alt="Beautiful View"
+                                                    className="blogThumbnail"
+                                                    image={blog.media}
+                                                    title="Beautiful View"
+                                                />
+                                            </Grid>
+                                            <Grid className="responsiveCardBottom" item xs={12} sm={12} md={6}>
+                                                <CardContent style={{paddingBottom: 0}}>
+                                                    <div style={{height: "80%", marginBottom: 'auto'}}>
+                                                        <Typography gutterBottom className="classesH2" variant="h5" component="h2">
+                                                        {blog.title}
+                                                        </Typography>
+                                                        <Typography gutterBottom variant="body2" color="textSecondary" component="p">
+                                                        {blog.summary}
+                                                        </Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Typography style={{textAlign: 'right'}} gutterBottom variant="overline" color="textSecondary" component="p">
+                                                        {blog.date}
+                                                        </Typography>
+                                                    </div>
+                                                </CardContent>
+                                            </Grid>
+                                        </Grid>
+                                    </CardActionArea>
+                                </A>
+                            </Card>
+                        ))}
+                    </Grid>
+                </div>
+            </Grid>
+        )
+    }
+    
 }
 
 export default Blog;
--- src/BlogPage/BlogPage.js
+++ src/BlogPage/BlogPage.js
@@ -1,10 +1,91 @@
 import React from "react";
+import blogData from "../data/blogPosts";
+import uuid from "uuid";
+import { DiscussionEmbed } from 'disqus-react';
+import './Blog.css';
 
-function BlogPage() {
+import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
+
+// Replace all special characters with _
+function replaceAll(str, replace) {
+    return str.replace(/[^A-Z0-9]+/ig, replace);
+}
+
+function BlogPage(props) {
+    let blogPost = {};
+    // Look through blogPosts.json to find the specific blog post.
+    for (let i = 0; i < blogData.blogPosts.length; i++) {
+        if (replaceAll(blogData.blogPosts[i].title, '_') === props.id) {
+            blogPost = blogData.blogPosts[i];
+            break;
+        }
+    }
+
+    const url = 'https://covid-19-au.com/blog/' + props.id;
+
     return (
-        <div>
-            
-        </div>
+        <Grid item xs={11} sm={11} md={10}>
+            <div className="card">
+                <div style={{justifyContent: 'center', display: 'flex'}}>
+                    <Paper className="blogImage" variant="outlined" elevation={3}>
+                        <img style={{height: '100%', width: '100%', objectFit: 'cover'}} src={blogPost.media} alt={blogPost.title} />
+                    </Paper>
+                </div>
+                <br />
+                <h2 className="responsiveH2">{blogPost.title}</h2>
+                <small className="blogDateText" style={{textAlign: 'right'}}>Posted on: {blogPost.date}</small>
+                <div className="blogText">
+                    
+                    {blogPost.text.text_1.length !== 0 ? (
+                        <div className="blogSection">
+                            {blogPost.text.text_1.map(t1 => (
+                                <p dangerouslySetInnerHTML={{ __html: t1 }} key={uuid()} />
+                            ))}
+                        </div>
+                    ) : ""}
+                    
+                    {blogPost.text.ulist_1.length !== 0 ? (
+                        <div className="blogSection">
+                            <ul>
+                            {blogPost.text.ulist_1.map(u1 => (
+                                <li dangerouslySetInnerHTML={{ __html: u1 }} key={uuid()} />
+                            ))}
+                            </ul>
+                        </div>
+                    ) : ""}
+
+                    {blogPost.text.olist_1.length !== 0 ? (
+                        <div className="blogSection">
+                            <ol>
+                            {blogPost.text.olist_1.map(o1 => (
+                                <li dangerouslySetInnerHTML={{ __html: o1 }} key={uuid()} />
+                            ))}
+                            </ol>
+                        </div>
+                    ) : ""}
+                    
+                    {blogPost.text.text_2.length !== 0 ? (
+                        <div className="blogSection">
+                            {blogPost.text.text_2.map(t2 => (
+                                <p dangerouslySetInnerHTML={{ __html: t2 }} key={uuid()} />
+                            ))}
+                        </div>
+                    ) : ""}
+                    
+                </div>
+                <DiscussionEmbed
+                shortname='covid-19-au'
+                config={
+                    {
+                        url: url,
+                        identifier: blogPost.title,
+                        title: blogPost.title,
+                    }
+                }
+                />
+            </div>
+        </Grid>
     )
 }
 
