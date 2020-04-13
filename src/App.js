851b05ea6e2792ae2e3ba919b84dd08896bef6e2
Paldeep <penguinpp93@gmail.com>
2020-04-14 00:45:28 +1000

    dropbox change

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function Header({ province }) {
           fontSize: "170%",
           color: "white",
           textAlign: "center",
-          fontWeight:"bold"
+          fontWeight: "bold"
         }}
       >
         {i18next.t("homePage:misc.title")}
@@ -99,7 +99,7 @@ function Header({ province }) {
           fontSize: "160%",
           color: "white",
           textAlign: "center",
-            fontWeight:"bold"
+          fontWeight: "bold"
         }}
       >
         {i18next.t("homePage:misc.descrip")}
@@ -108,45 +108,45 @@ function Header({ province }) {
 
       <div className="slogan"><i>{i18next.t("homePage:misc.slogan")}</i></div>
 
-        <div style={{
-            fontSize: "120%",
-            color: "white",
-            textAlign: "center",
-            marginTop:"1rem"
-        }}>
-            <SocialMediaShareModal
-                visible={showSocialMediaIcons}
-                onCancel={() => setShowSocialMediaIcons(false)}
-            />
-            <a  onClick={() => {
-                ReactGA.event({category: 'Header', action: "share"});
-                setModalVisibility(true)
-            }}><i className="fas fa-share-alt"></i></a>
-            <a style={{marginLeft:'0.5rem'}} target="_blank" rel="noopener noreferrer" onClick={() => {ReactGA.event({category: 'Header', action: "twitter"})}} href="https://twitter.com/covid19augithub"><i className="fab fa-twitter"></i></a>
-            <a style={{marginLeft:'0.5rem'}} target="_blank" rel="noopener noreferrer" onClick={() => {ReactGA.event({category: 'Header', action: "instagram"})}} href="https://www.instagram.com/covid19_au/"><i className="fab fa-instagram"></i></a>
-            <a style={{marginLeft:'0.5rem'}} target="_blank" rel="noopener noreferrer" onClick={() => {ReactGA.event({category: 'Header', action: "github"})}} href="https://www.facebook.com/covid19au.github/"><i className="fab fa-facebook"></i></a>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              {i18next.t("nav:lang")}
-              </button>
-                <div className="dropdown-menu" >
-                <a className="dropdown-item" onClick={changeLanguage('en')}>English</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('es')}>Español</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('vi')}>Tiếng Việt</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('zh')}>简体中文</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('tw')}>繁體中文</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('ko')}>한국어</a>
-                    <div className="dropdown-divider"></div>
-                <a className="dropdown-item" onClick={changeLanguage('ja')}>日本語</a>
-              </div>
-            </div>
+      <div style={{
+        fontSize: "120%",
+        color: "white",
+        textAlign: "center",
+        marginTop: "1rem"
+      }}>
+        <SocialMediaShareModal
+          visible={showSocialMediaIcons}
+          onCancel={() => setShowSocialMediaIcons(false)}
+        />
+        <a onClick={() => {
+          ReactGA.event({ category: 'Header', action: "share" });
+          setModalVisibility(true)
+        }}><i className="fas fa-share-alt"></i></a>
+        <a style={{ marginLeft: '0.5rem' }} target="_blank" rel="noopener noreferrer" onClick={() => { ReactGA.event({ category: 'Header', action: "twitter" }) }} href="https://twitter.com/covid19augithub"><i className="fab fa-twitter"></i></a>
+        <a style={{ marginLeft: '0.5rem' }} target="_blank" rel="noopener noreferrer" onClick={() => { ReactGA.event({ category: 'Header', action: "instagram" }) }} href="https://www.instagram.com/covid19_au/"><i className="fab fa-instagram"></i></a>
+        <a style={{ marginLeft: '0.5rem' }} target="_blank" rel="noopener noreferrer" onClick={() => { ReactGA.event({ category: 'Header', action: "github" }) }} href="https://www.facebook.com/covid19au.github/"><i className="fab fa-facebook"></i></a>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ backgroundColor: "transparent", border: "none", outline: "none" }}>
+            {i18next.t("nav:lang")}
+          </button>
+          <div className="dropdown-menu" >
+            <a className="dropdown-item" onClick={changeLanguage('en')}>English</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('es')}>Español</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('vi')}>Tiếng Việt</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('zh')}>简体中文</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('tw')}>繁體中文</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('ko')}>한국어</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" onClick={changeLanguage('ja')}>日本語</a>
+          </div>
         </div>
+      </div>
 
       {/*<i>By Students from Monash</i>*/}
     </header>
@@ -311,7 +311,7 @@ function App() {
       <div>
         <SocialMediaShareModal
           visible={showSocialMediaIcons}
-          onCancel={ () => setShowSocialMediaIcons(false)}
+          onCancel={() => setShowSocialMediaIcons(false)}
         />
         <Grid container spacing={gspace} justify="center" wrap="wrap">
           <Grid item xs={12} className="removePadding">
