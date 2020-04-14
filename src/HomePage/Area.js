750bf90148f1c6e8d7f8321147c7ac1b8933a5fe
Han <han@karma.wiki>
2020-04-14 22:02:20 +0800

    daily update

--- src/HomePage/Area.js
+++ src/HomePage/Area.js
@@ -118,7 +118,7 @@ export default function Area({ area, onChange, data }) {
           {numberWithCommas(sumRow(CONFIRMED, data))}
         </div>
         <div className="death">{numberWithCommas(sumRow(DEATH, data))}</div>
-        <div className="cured">*3400+</div>
+        <div className="cured">*3500+</div>
         <div className="tested">{numberWithCommas(sumRow(TESTED, data))}</div>
       </div>
     );
