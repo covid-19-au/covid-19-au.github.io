7086388f1643ee2d8c6b165af645077be715694f
wooyunlong <wooyunlong@gmail.com>
2020-04-17 15:12:48 +1000

    Add Ships Function

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,84 +1,76 @@
-
 import React, { useState, Suspense, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 
 import keyBy from "lodash.keyby";
 import Area from "./Area";
 import Flights from "./Flights";
-import StateGraph from "./StateGraph";
 import MbMap from "./ConfirmedMap";
-import Stat from "./Stat"
-import HistoryGraph from "./HistoryGraph"
-import EChartGlobalLog from "./EChartGlobalLog"
+import Stat from "./Stat";
+import EChartGlobalLog from "./EChartGlobalLog";
 
-import uuid from "react-uuid";
-import CanvasJSReact from "../assets/canvasjs.react";
 import provinces from "../data/area";
-import stateData from "../data/state";
 import flights from "../data/flight";
 import country from "../data/country";
 import all from "../data/overall";
 
-import OverallTrend from "./OverallTrend"
-import StateComparisonChart from "./StateComparisonChart"
+import OverallTrend from "./OverallTrend";
+import StateComparisonChart from "./StateComparisonChart";
 
+import Ships from "./Ships";
 
 const provincesByName = keyBy(provinces, "name");
 
 const GoogleMap = React.lazy(() => import("./GoogleMap"));
 
 export default function HomePage({
-    province,
-    overall,
-    myData,
-    area,
-    data,
-    setProvince,
-    gspace
+  province,
+  overall,
+  myData,
+  area,
+  data,
+  setProvince,
+  gspace,
 }) {
-    return (
-        <Grid container spacing={gspace} justify="center" wrap="wrap">
-            <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
+  return (
+    <Grid container spacing={gspace} justify="center" wrap="wrap">
+      <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
+        <Stat
+          {...{ ...all, ...overall }}
+          name={province && province.name}
+          data={myData}
+          countryData={country}
+        />
+        <div className="card">
+          <Suspense fallback={<div className="loading">Loading...</div>}>
+            <GoogleMap
+              province={province}
+              data={data}
+              onClick={(name) => {
+                const p = provincesByName[name];
+                if (p) {
+                  setProvince(p);
+                }
+              }}
+              newData={myData}
+            />
+            <Area area={area} onChange={setProvince} data={myData} />
+          </Suspense>
+        </div>
+      </Grid>
 
+      <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
+        <MbMap />
+        <OverallTrend />
+      </Grid>
+      <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
+        <StateComparisonChart />
+        <EChartGlobalLog />
+      </Grid>
 
-                <Stat
-                    {...{ ...all, ...overall }}
-                    name={province && province.name}
-                    data={myData}
-                    countryData={country}
-                />
-                <div className="card" >
-                    <Suspense fallback={<div className="loading">Loading...</div>}>
-                        <GoogleMap
-                            province={province}
-                            data={data}
-                            onClick={name => {
-                                const p = provincesByName[name];
-                                if (p) {
-                                    setProvince(p);
-                                }
-                            }}
-                            newData={myData}
-                        />
-                        <Area area={area} onChange={setProvince} data={myData} />
-                    </Suspense>
-                </div>
-            </Grid>
-
-            <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
-                <MbMap />
-                <OverallTrend />
-            </Grid>
-            <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
-                <StateComparisonChart />
-                <EChartGlobalLog />
-            </Grid>
-
-            <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
-                <Flights flights={flights} />
-            </Grid>
-
-
-        </Grid>
-    );
+      <Grid item xs={11} sm={11} md={10} lg={6} xl={4}>
+        <Flights flights={flights} />
+        <Ships />
+      </Grid>
+    </Grid>
+  );
 }
