f587234f56063f79068525639edc8e115d498922
Paldeep <penguinpp93@gmail.com>
2020-04-20 14:58:22 +1000

    languaghe dropdown

--- src/socialMediaShare/SocialMediaShareModal.js
+++ src/socialMediaShare/SocialMediaShareModal.js
@@ -4,20 +4,21 @@ import Modal from 'react-modal';
 import SharedButtons from './SharedButtons';
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
+
 };
 
 Modal.setAppElement('#root')
 
 const SocialMediaShareModal = ({ visible, onCancel }) => {
-  return(
+  return (
     <Modal
       isOpen={visible}
       onRequestClose={onCancel}
