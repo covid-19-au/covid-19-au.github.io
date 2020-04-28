81379fbc79f7140b483fe6d6c7c9e251727b805d
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-28 15:33:39 +1000

    minified geojson files, removing some unused properties+reducing float precision

--- src/data/geojson/minify_geojson.py
+++ src/data/geojson/minify_geojson.py
@@ -4,13 +4,18 @@ from os import listdir
 
 def get_minified(path):
     with open(path, 'r', encoding='utf-8') as f:
-        geojson = json.loads(f.read())
+        geojson = json.loads(
+            f.read(),
+            parse_float=lambda x: round(float(x), 3)
+        )
 
     new_features = []
     for feature in geojson['features']:
         if not feature['geometry']:
             continue
-        
+        elif 'unincorporated' in str(feature).lower():
+            continue
+
         for key in (
             'id',
             'lg_ply_pid',
@@ -19,13 +24,18 @@ def get_minified(path):
             'lga_pid',
             'tas_lga_sh',
         ):
-            if key in feature:
-                del feature[key]
+            if key in feature['properties']:
+                del feature['properties'][key]
+        new_features.append(feature)
 
+    geojson['features'] = new_features
 
+    encoded = json.dumps(geojson, separators=(',', ':'))
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(encoded)
 
 
 for fnam in listdir('.'):
     if not fnam.endswith('.geojson'):
         continue
-
+    get_minified(fnam)
