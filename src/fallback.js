989e44f0b4f995f285ab82708e7048ae89490e99
Han <han@karma.wiki>
2020-08-18 22:21:37 +0800

    update

--- src/fallback.js
+++ src/fallback.js
@@ -2,53 +2,75 @@ import React from "react";
 import ReactGA from "react-ga";
 import { A } from "hookrouter";
 import Button from "@material-ui/core/Button";
-import InfoSharpIcon from '@material-ui/icons/InfoSharp';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import PeopleIcon from '@material-ui/icons/People';
-import FeedbackIcon from '@material-ui/icons/Feedback';
-import ShareIcon from '@material-ui/icons/Share';
-import { makeStyles } from '@material-ui/core/styles';
+import InfoSharpIcon from "@material-ui/icons/InfoSharp";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import PeopleIcon from "@material-ui/icons/People";
+import FeedbackIcon from "@material-ui/icons/Feedback";
+import ShareIcon from "@material-ui/icons/Share";
+import { makeStyles } from "@material-ui/core/styles";
+import PrivacyPolicy from "./PrivacyPolicy.js";
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: "2px",
     backgroundColor: "white",
     variant: "outlined",
-    textTransform: "none"
+    textTransform: "none",
   },
 }));
 
-
 // ReactGA.initialize("UA-160673543-1",{gaoOptions:{siteSpeedSampleRate: 100}});
 function Fallback(props) {
-
-
   const classes = useStyles();
 
   return (
-
-
     <div className="fallback">
       <div>
-        <Button onClick={() => {
-          ReactGA.event({ category: "Fallback", action: "share" });
-          props.setModalVisibility(true);
-        }} variant="outlined" startIcon={<ShareIcon />} className={classes.button}>
+        <Button
+          onClick={() => {
+            ReactGA.event({ category: "Fallback", action: "share" });
+            props.setModalVisibility(true);
+          }}
+          variant="outlined"
+          startIcon={<ShareIcon />}
+          className={classes.button}
+        >
           Share this site
         </Button>
 
-        <Button href="https://docs.google.com/forms/d/e/1FAIpQLSeX4RU-TomFmq8HAuwTI2_Ieah60A95Gz4XWIMjsyCxZVu7oQ/viewform?usp=sf_link" variant="outlined" startIcon={<FeedbackIcon />} className={classes.button}>
+        <Button
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeX4RU-TomFmq8HAuwTI2_Ieah60A95Gz4XWIMjsyCxZVu7oQ/viewform?usp=sf_link"
+          variant="outlined"
+          startIcon={<FeedbackIcon />}
+          className={classes.button}
+        >
           Feedback
         </Button>
-        <Button href="/faq" variant="outlined" startIcon={<InfoSharpIcon />} className={classes.button}>
+        <Button
+          href="/faq"
+          variant="outlined"
+          startIcon={<InfoSharpIcon />}
+          className={classes.button}
+        >
           FAQ
         </Button>
-        <Button href="https://github.com/covid-19-au/covid-19-au.github.io" variant="outlined" startIcon={<GitHubIcon />} className={classes.button}>
+        <Button
+          href="https://github.com/covid-19-au/covid-19-au.github.io"
+          variant="outlined"
+          startIcon={<GitHubIcon />}
+          className={classes.button}
+        >
           GitHub
         </Button>
-        <Button href="/about-us" variant="outlined" startIcon={<PeopleIcon />} className={classes.button}>
+        <Button
+          href="/about-us"
+          variant="outlined"
+          startIcon={<PeopleIcon />}
+          className={classes.button}
+        >
           About Us
         </Button>
+        <PrivacyPolicy></PrivacyPolicy>
       </div>
       <div>Template credits to: shfshanyue</div>
 
@@ -135,14 +157,22 @@ function Fallback(props) {
           />
         </svg>
 
-
         <div id="sfcly4m9kmug45da8x8ba6fchlharqeeqm1"></div>
-        <script type="text/javascript" src="https://counter3.stat.ovh/private/counter.js?c=ly4m9kmug45da8x8ba6fchlharqeeqm1&down=async" async></script>
-        <a href="https://www.freecounterstat.com" title="free hit counter"><img src="https://counter3.stat.ovh/private/freecounterstat.php?c=ly4m9kmug45da8x8ba6fchlharqeeqm1" border="0" title="free hit counter" alt="free hit counter" /></a>
-
-
+        <script
+          type="text/javascript"
+          src="https://counter3.stat.ovh/private/counter.js?c=ly4m9kmug45da8x8ba6fchlharqeeqm1&down=async"
+          async
+        ></script>
+        <a href="https://www.freecounterstat.com" title="free hit counter">
+          <img
+            src="https://counter3.stat.ovh/private/freecounterstat.php?c=ly4m9kmug45da8x8ba6fchlharqeeqm1"
+            border="0"
+            title="free hit counter"
+            alt="free hit counter"
+          />
+        </a>
       </div>
-    </div >
+    </div>
   );
 }
 
